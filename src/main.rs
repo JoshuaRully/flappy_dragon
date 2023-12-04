a0f495f8a65d8de6af093d71c3d934a5e679e00a
Joshua Rully <joshrully@gmail.com>
2023-12-03 17:57:28 -0800

    [main] Implement obstacle and finish game

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ enum GameMode {
     End,
 }
 
-const SCREEN_WIDTH : i32 = 80;
-const SCREEN_HEIGHT : i32 = 50;
-const FRAME_DURATION : f32 = 75.0;
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
 
 struct Player {
     x: i32,
@@ -49,6 +49,8 @@ struct State {
     mode: GameMode,
     frame_time: f32,
     player: Player,
+    obstacle: Obstacle,
+    score: i32,
 }
 
 impl State {
@@ -57,6 +59,8 @@ impl State {
             mode: GameMode::Menu,
             frame_time: 0.0,
             player: Player::new(5, 25),
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
+            score: 0,
         }
     }
 
@@ -73,7 +77,14 @@ impl State {
         }
         self.player.render(ctx);
         ctx.print(0, 0, "Press SPACE to flap.");
-        if self.player.y > SCREEN_HEIGHT {
+        ctx.print(0, 1, &format!("Score: {}", self.score));
+
+        self.obstacle.render(ctx, self.player.x);
+        if self.player.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score)
+        }
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
             self.mode = GameMode::End;
         }
     }
@@ -81,7 +92,9 @@ impl State {
     fn restart(&mut self) {
         self.player = Player::new(5, 25);
         self.frame_time = 0.0;
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
         self.mode = GameMode::Playing;
+        self.score = 0;
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
@@ -102,6 +115,7 @@ impl State {
     fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(6, &format!("You earned {} points", self.score));
         ctx.print_centered(8, "(P) Play Again");
         ctx.print_centered(5, "(Q) Quit Game");
 
@@ -118,13 +132,17 @@ impl State {
 struct Obstacle {
     x: i32,
     gap_y: i32,
-    size: i32
+    size: i32,
 }
 
 impl Obstacle {
     fn new(x: i32, score: i32) -> Self {
         let mut random = RandomNumberGenerator::new();
-        Obstacle { x, gap_y: random.range(10, 40), size: i32::max(2, 20 - score) }
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40),
+            size: i32::max(2, 20 - score),
+        }
     }
 
     fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
@@ -132,23 +150,11 @@ impl Obstacle {
         let half_size = self.size / 2;
 
         for y in 0..self.gap_y - half_size {
-            ctx.set(
-                screen_x,
-                y,
-                RED,
-                BLACK,
-                to_cp437('|'),
-            )
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'))
         }
 
         for y in self.gap_y + half_size..SCREEN_HEIGHT {
-            ctx.set(
-                screen_x,
-                y,
-                RED,
-                BLACK,
-                to_cp437('|'),
-            )            
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'))
         }
     }
 
